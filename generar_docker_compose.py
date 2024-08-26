f1a87d8c188eda4094b34c8a34c653c18405ba07
Kevin Gadacz <kgadacz@fi.uba.ar>
2024-08-26 08:22:56 -0300

    feature: se agrega volumes al docker compose

--- generar_docker_compose.py
+++ generar_docker_compose.py
@@ -10,6 +10,8 @@ services:
     environment:
       - PYTHONUNBUFFERED=1
       - LOGGING_LEVEL=DEBUG
+    volumes:
+      - ./server/config.ini:/config.ini
     networks:
       - testing_net
 """
@@ -25,6 +27,8 @@ services:
     environment:
       - CLI_ID={i}
       - CLI_LOG_LEVEL=DEBUG
+    volumes:
+      - ./client/config.yaml:/config.yaml
     networks:
       - testing_net
     depends_on:
