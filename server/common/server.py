639db98e5299b409131a69016b7f24699f686429
Kevin Gadacz <kgadacz@fi.uba.ar>
2024-09-05 08:48:22 -0300

    feat: agrego multiprocessing

--- server/common/server.py
+++ server/common/server.py
@@ -2,24 +2,30 @@ import socket
 import logging
 import signal
 import sys
-
+import multiprocessing
 from common.protocol import send_message, receive_message
 from common.utils import store_bets, load_bets, has_won
 from common.bet_parser import parse_bets
 from common.constants import (
-    AMOUNT_OF_CLIENTS,
     BET_TYPE_MESSAGE, WINNER_TYPE_MESSAGE,
     OK_RESPONSE, ERROR_RESPONSE, REFUSED_RESPONSE
 )
 
 class Server:
-    def __init__(self, port, listen_backlog):
+    def __init__(self, port, listen_backlog, amount_of_clients):
         # Initialize server socket
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
         self._active_connections = []
-        self._clients_bets_finished = 0
+        self.processes = []
+        self._amount_of_clients = amount_of_clients
+        # Create a manager for shared data and lock
+        manager = multiprocessing.Manager()
+        self._clients_bets_finished = manager.Value('i', 0)
+        self._clients_bets_finished_lock = manager.Lock()
+        self._store_bets_lock = manager.Lock()
+        
         signal.signal(signal.SIGINT, self.__handle_signal)
         signal.signal(signal.SIGTERM, self.__handle_signal)
 
@@ -39,20 +45,26 @@ class Server:
         logging.debug("action: close server socket | result: success")
         
         logging.info(f"action: signal {signum} | result: success")
+
+        for process in self.processes:
+            process.join()
+        logging.debug(f'action: join process | result: success')
+
         sys.exit(0)
 
     def run(self):
         """
-        Dummy Server loop
+        Server loop
 
-        Server that accepts new connections and establishes a
-        communication with a client. After the communication
-        finishes, the server starts to accept new connections again.
+        Accepts new connections and spawns a process to handle each client.
         """
-
         while True:
             client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+            client_proccess = multiprocessing.Process(
+                target=self.__handle_client_connection,
+                args=(client_sock,))
+            client_proccess.start()
+            self.processes.append(client_proccess)
 
     def __handle_client_bets(self, client_sock):
         count_chunks = int(receive_message(client_sock))
@@ -69,10 +81,16 @@ class Server:
                 error_processing_chunks = True
                                 
             cantidad_apuestas += len(bets)
-            for bet in bets:
-                store_bets([bet])
-
-        self._clients_bets_finished += 1
+            
+            # Critical section: store bets (synchronized with lock)
+            logging.info("pido el lock para store_bets")
+            with self._store_bets_lock:
+                store_bets([bet for bet in bets])
+            logging.info("libero el lock para store_bets")
+        logging.info("pido el lock para incrementar el contador de apuestas")
+        with self._clients_bets_finished_lock:
+            self._clients_bets_finished.value += 1
+        logging.info("libero el lock para incrementar el contador de apuestas")
         if error_processing_chunks:
             logging.error(f'apuestas_recibidas | result: fail | cantidad: {cantidad_apuestas}')
         else:
@@ -80,17 +98,21 @@ class Server:
 
     def __handle_ask_client_winner(self, client_sock):
         id = int(receive_message(client_sock))
-
-        if self._clients_bets_finished < AMOUNT_OF_CLIENTS:
-            logging.error("action: sorteo | result: fail")
-            send_message(client_sock, REFUSED_RESPONSE)
-        else:
-            logging.info("action: sorteo | result: success")
-            amount_winners = self.__handle_get_amount_winners(id)
-            send_message(client_sock, str(amount_winners))
+        # Check if all clients have finished placing bets (synchronized with lock)
+        with self._clients_bets_finished_lock:
+            logging.debug("Usando lock para sorteo en el cliente {}".format(id))
+            if self._clients_bets_finished.value < self._amount_of_clients:
+                logging.error("action: sorteo | result: fail")
+                send_message(client_sock, REFUSED_RESPONSE)
+            else:
+                logging.info("action: sorteo | result: success")
+                amount_winners = self.__handle_get_amount_winners(id)
+                send_message(client_sock, str(amount_winners))
+        logging.debug("Libero el lock para sorteo en el cliente {}".format(id))
 
     def __handle_get_amount_winners(self, id):
         winners = 0
+        
         for bet in load_bets():
             if has_won(bet) and bet.agency == id:
                 winners += 1
@@ -98,10 +120,7 @@ class Server:
 
     def __handle_client_connection(self, client_sock):
         """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed.
+        Handle communication with a client in a separate process.
         """
         self._active_connections.append(client_sock)
 
@@ -121,13 +140,10 @@ class Server:
 
     def __accept_new_connection(self):
         """
-        Accept new connections
+        Accept new connections.
 
-        Function blocks until a connection to a client is made.
-        Then the connection is printed and returned.
+        Blocks until a connection to a client is made.
         """
-
-        # Connection arrived
         logging.info('action: accept_connections | result: in_progress')
         c, addr = self._server_socket.accept()
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
